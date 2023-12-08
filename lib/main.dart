cdda8aa185597c159d06b2078965236bc1cba2f0
Ashik <ashik.searchg@gmail.com>
2023-12-08 23:16:17 +0600

    assigment added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
-void main(){
-  print("Hello java");
+import 'dart:io';
+import 'dart:math';
+
+void main()
+{
+
+  double? R=double.parse(stdin.readLineSync()!);
+  double pi=3.14159;
+  double volume=4.0/3*pi*R*R*R;
+  print('VOLUME = ${volume.toStringAsFixed(3)}');
 
 
 }
